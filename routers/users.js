048bb7982847b83d65f7bb7e9c318c467f2b18e1
Anna Basarab <90757241+AnnaBasarab93@users.noreply.github.com>
2023-09-30 13:53:50 +0200

    added Validate for users

--- routers/users.js
+++ routers/users.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const usersRouter = express.Router();
-
+import { check, validationResult } from 'express-validator';
 import pg from 'pg';
 const { Pool } = pg;
 
@@ -32,7 +32,16 @@ usersRouter.get("/:id", async (req, res) => {
 
 //POST / -> To create a new user 
 
-usersRouter.post("/", async (req, res) => {
+usersRouter.post("/",[
+    check('first_name').notEmpty().isString(),
+    check('last_name').notEmpty().isString(),
+    check('age').notEmpty().isString(),
+], async (req, res) => {
+    const errors = validationResult(req);
+        if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    };
+
     const {first_name, last_name, age} = req.body;
     try {
         const {rows} = await pool.query('INSERT INTO users(first_name, last_name, age) VALUES($1, $2, $3) RETURNING *;', [first_name, last_name, age]);
@@ -45,10 +54,19 @@ usersRouter.post("/", async (req, res) => {
 
 //PUT /:id  :  To edit one user (with the id) 
 
-usersRouter.put("/:id", async (req, res) => {
+usersRouter.put("/:id",[
+    check('first_name').notEmpty().isString(),
+    check('last_name').notEmpty().isString(),
+    check('age').notEmpty().isString(),
+], async (req, res) => {
+    const errors = validationResult(req);
+        if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+        };
+        
     const {first_name, last_name, age} = req.body;
     const {id} = req.params;
-    try {
+        try {
         const {rows} = await pool.query('UPDATE users SET first_name=$1, last_name=$2, age=$3  WHERE id=$4 RETURNING *;', [first_name, last_name, age, id]);
         res.json(rows[0])
 
