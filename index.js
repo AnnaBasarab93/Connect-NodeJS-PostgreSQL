8b11e2fdebe2c8bd3c312df202383692e477f25f
Anna Basarab <90757241+AnnaBasarab93@users.noreply.github.com>
2023-09-30 11:47:45 +0200

    Done Users API

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import usersRouter from './routers/users.js'
+import ordersRouter from './routers/oders.js';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/users', usersRouter);
+app.use ('api/orders', ordersRouter)
+
+
+
+
+
+
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
---
+++ routers/users.js
@@ -0,0 +1,73 @@
+import express from 'express';
+const usersRouter = express.Router();
+
+import pg from 'pg';
+const { Pool } = pg;
+
+const pool = new Pool();
+
+//GET  /  : To get all the users 
+
+usersRouter.get("/", async(req, res) => {
+    try{
+        const {rows} = await pool.query('SELECT * FROM users');
+        res.json(rows)
+    }catch(error) {
+        res.status(500).json(error)
+    }
+})
+
+// GET  /:id :  To get one user (with the id) 
+usersRouter.get("/:id", async (req, res) => {
+    const {id} = req.params;
+        try{
+            const {rows} = await pool.query('SELECT * FROM users WHERE id=$1;', [id])
+            console.log(rows[0])
+            res.json(rows[0])
+
+        }catch(error){
+        res.status(500).json(error)
+    }
+})
+
+//POST / -> To create a new user 
+
+usersRouter.post("/", async (req, res) => {
+    const {first_name, last_name, age} = req.body;
+    try {
+        const {rows} = await pool.query('INSERT INTO users(first_name, last_name, age) VALUES($1, $2, $3) RETURNING *;', [first_name, last_name, age]);
+        res.json(rows[0])
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//PUT /:id  :  To edit one user (with the id) 
+
+usersRouter.put("/:id", async (req, res) => {
+    const {first_name, last_name, age} = req.body;
+    const {id} = req.params;
+    try {
+        const {rows} = await pool.query('UPDATE users SET first_name=$1, last_name=$2, age=$3  WHERE id=$4 RETURNING *;', [first_name, last_name, age, id]);
+        res.json(rows[0])
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//DELETE  /:id : To delete one order (with the id) 
+
+usersRouter.delete("/:id", async (req, res) => {
+    const {id} = req.params;
+    try {
+        const {rows} = await pool.query('DELETE FROM users WHERE id=$1 RETURNING *;', [id]);
+        res.json(rows[0])
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+export default usersRouter;
